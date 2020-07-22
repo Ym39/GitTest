212a029e6ee421fa9dfd885bfcd42b1546d7214e
DESKTOP-R3LR6F4\admin <dpals39@gmail.com>
2020-07-22 10:07:34 +0900

    no message

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     private int test;
     private int master = 1;
     private int testBranche = 1;
-    private float speed = 100f;
+    private float hp = 100f;
     void Start()
     {
         
